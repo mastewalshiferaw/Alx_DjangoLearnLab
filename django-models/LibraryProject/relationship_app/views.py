19be69a605ec1731335724d53538a15c48af2fa9
Mastewal <mastewalgshiferaw@gmail.com>
2025-07-17 23:29:45 +0300

    committed task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView 
+from .models import Book, Library          
+def list_all_books(request):
+   
+    all_books = Book.objects.all()
+    response_lines = []
+    for book in all_books:
+        line = f"{book.title} by {book.author.name}"
+        response_lines.append(line)
+    response_text = "\n".join(response_lines)
+    return HttpResponse(response_text, content_type="text/plain")
 
-# Create your views here.
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+class LibraryDetailView(DetailView):
+   
+    model = Library
+
+    
+    template_name = 'relationship_app/library_detail.html'
+    
+    context_object_name = 'library'
