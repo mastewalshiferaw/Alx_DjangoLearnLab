31754604f7bdcdbbe3442be58a663b8c66d5ffed
Mastewal <mastewalgshiferaw@gmail.com>
2025-07-17 22:35:49 +0300

    Committed task 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Library, Book, Librarian
+from relationship_app.models import Author, Book, Library, Librarian
 
 
 author = Author.object.get(name = "Abebe")
@@ -8,13 +8,9 @@ for book in books:
   print(book.title)
 
 library_name = "Central Library"
-library = Library.objects.get(name =library_name)
+library = Library.objects.get(name=library_name) 
 books_in_library = library.books.all()
 
 
-for book in books:
-    print(book.title)
-
-library = Library.objects.get(id=1)
-librarian = library.librarian  
-print(librarian.name)
+library_for_librarian = Library.objects.get(id=1)
+librarian = library_for_librarian.librarian
