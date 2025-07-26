522521810f04db4e47be5795192566fa2a621118
Mastewal <mastewalgshiferaw@gmail.com>
2025-07-26 17:07:39 +0300

    Task 0, Week 11 committed

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Book
 
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Information', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
 class BookAdmin(admin.ModelAdmin):
   list_Display = ('title', 'author', 'publication_year')
   list_filter =('author', 'publication_year')
