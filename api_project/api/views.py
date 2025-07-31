6f639321576d55a408fcad5112fbba70faa8b6ec
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-01 01:47:50 +0300

    Committed TASK 1, WEEK 12

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generic import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
