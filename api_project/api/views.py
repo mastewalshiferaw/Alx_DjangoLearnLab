ea77d779c3b8ad49129f183a1aa2925ff24dab23
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-01 21:50:37 +0300

    Committed TASK 2, WEEK 12

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
 
-from rest_framework.generic import ListAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
