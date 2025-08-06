3d33d720f5641380af47ad87eb326cb8439f7d7b
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-07 01:53:48 +0300

    Committed Task 1, week 13(implemented new view)

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView, AuthorListCreateView, AuthorDetailView
-
+from .views import BookListCreateView, BookDetailView
 # --- Documentation for URL Patterns ---
 # The URL patterns map specific HTTP endpoints to the views we defined.
 
@@ -10,7 +9,5 @@ urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 
-    # Author URLs
-    path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    
 ]
