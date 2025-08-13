cff37511e641dbd1ed6f90b77c4e0efe59f908ae
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-14 02:17:17 +0300

    Initial commit of task 2, week 14

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm 
+from .forms import CustomUserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+
 
 # --- Registration View ---
 def register(request):
@@ -42,3 +53,50 @@ def profile(request):
         'u_form': u_form
     }
     return render(request, 'blog/profile.html', context)
+
+# This is a view to display all posts.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] 
+class PostDetailView(DetailView):
+    model = Post
+
+# This view allows a logged-in user to create a new post.
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content'] # Fields the user can fill in
+
+    # sets the author to the currently logged-in user.
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# This view allows the author of a post to update it.
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/' # Where to redirect after a successful deletion
+
+    # checks if the user trying to delete the post is the author.
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
