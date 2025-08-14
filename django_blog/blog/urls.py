a7d83f2f4eb98fa42bd6e6c262e671f63d38004c
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-14 23:56:40 +0300

    committed task 3, week 14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,41 +1,37 @@
-from django.urls import path
 
+from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views 
+from . import views
+
+
 from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
-    CommentCreateView,
+    CommentCreateView, 
     CommentUpdateView,
     CommentDeleteView
 )
 
-
 urlpatterns = [
-    # Path for our custom registration view
+    # --- Authentication URLs ---
     path('register/', views.register, name='register'),
-
-    # Path for Django's built-in LoginView.
-    
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-
-    # Path for  LogoutView.
-    
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-
-    # Path for custom profile view
     path('profile/', views.profile, name='profile'),
 
-        # Map each view to a URL
-    
-    path('', PostListView.as_view(), name='post-list'), # Set the home page to list all posts
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # --- Blog Post CRUD URLs ---
+    path('', PostListView.as_view(), name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # --- Comment CRUD URLs ---
+    
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create')
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
