bbaf22583bd0b56413e86faae5ef7adf6df08654
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-14 18:16:57 +0300

    committed task 3, week 14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create')
 ]
