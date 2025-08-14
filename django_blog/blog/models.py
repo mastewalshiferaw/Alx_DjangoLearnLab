6bc3b97915b67506081ea0b9741c777ed76b45e7
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-14 10:03:11 +0300

    Intial commit, implemented comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from django.utils import timezone
+
 
 
 class Post(models.Model):
@@ -15,3 +17,12 @@ class Post(models.Model):
   
   def get_absolute_url(self):
     return reverse('post-detail', kwargs={'pk':self.pk})
+  
+class Comment(models.Model):
+  post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
+  content = models.TextField(max_length=500)
+
+  created_at = models.DateTimeField(default=timezone.now)
+
+  updated_at = models.DateTimeField(default=timezone.now)
