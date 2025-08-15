adb4afe73e8fa7cac5a7889a796f215f56bd4b05
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-15 23:09:04 +0300

    Committed task 4, week 14, adusjusting the form adding TagWidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,22 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 from taggit.forms import TagWidget
 
-# Form for user registration.
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
 
-# Form for updating a user's profile.
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email']
 
-# Form for creating and updating blog posts.
-# THIS IS THE FORM THE CHECKER REQUIRES.
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -29,7 +25,6 @@ class PostForm(forms.ModelForm):
             'tags': TagWidget(attrs={'placeholder': 'Comma-separated tags'}),
         }
 
-# Form for adding comments to a post.
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
