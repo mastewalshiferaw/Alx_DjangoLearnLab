412b9992d16a5f04200053e6b4f892b15e8f9e66
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-19 03:11:03 +0300

    Imported Token and made adjustment on accounts\serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,7 +13,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ("username", "email", "password", "bio")
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
