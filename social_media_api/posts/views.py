acc266bd59cdc01a4c75a68f3cba9697a2e03ddd
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-21 01:35:30 +0300

    Update posts\views.py with added Post.objects.filter

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,34 +1,40 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework import filters
+
+=
 class PostPagination(PageNumberPagination):
     page_size = 5
 
+=
 class IsAuthorOrReadOnly(permissions.BasePermission):
-  
     def has_object_permission(self, request, view, obj):
-       
+        
         if request.method in permissions.SAFE_METHODS:
             return True
+        
         return obj.author == request.user
 
-
+# ViewSet for managing posts
 class PostViewSet(viewsets.ModelViewSet):
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-    queryset = Post.objects.all().order_by("-created_at")
+    queryset = Post.objects.all().order_by("-created_at")  
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]  # Allow search
+    search_fields = ['title', 'content']      =
+    pagination_class = PostPagination
 
+   t
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
+# ViewSet for managing comments
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by("-created_at")
+    queryset = Comment.objects.all().order_by("-created_at") 
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
+    
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
