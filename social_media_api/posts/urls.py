f5734da7ce73e99bb40b1dc681c70505062e84d1
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-21 23:39:59 +0300

    make a change posts/urls.py following checkers error

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,21 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
-
+from .views import PostViewSet, CommentViewSet, feed_view, like_post, unlike_post
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
-
 urlpatterns = [
+    
     path('', include(router.urls)),
-    path('feed/', feed, name='user-feed'),
-    path("post/<int:post_id>/like/", views.like_post, name="like_post"),
-    path("post/<int:post_id>/unlike/", views.unlike_post, name="unlike_post"),
 
-    path("<int:post_id>/like/", views.like_post, name="like_post"),
-    path("<int:post_id>/unlike/", views.unlike_post, name="unlike_post"),
-
+    path('feed/', feed_view, name='user-feed'),
+    path("<int:pk>/like/", like_post, name="like_post"),
+    path("<int:pk>/unlike/", unlike_post, name="unlike_post"),
 ]
-
